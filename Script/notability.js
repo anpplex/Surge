a52f89ae28a0351cf0a9e7c8d8a7d3604ed5f834
anpplex <35098199+anpplex@users.noreply.github.com>
2024-09-08 13:11:57 +0800

    Create notability.js

---
+++ Script/notability.js
@@ -0,0 +1,28 @@
+var anpple = JSON.parse($response.body);
+
+anpple = {
+   "data" : {
+     "processAppleReceipt" : {
+       "error" : 0,
+       "subscription" : {
+         "productId" : "com.gingerlabs.Notability.premium_subscription",
+         "originalTransactionId" : "570001184068302",
+         "tier" : "premium",
+         "refundedDate" : null,
+         "refundedReason" : null,
+         "isInBillingRetryPeriod" : false,
+         "expirationDate" : "2999-09-09T09:09:09.000Z",
+         "gracePeriodExpiresAt" : null,
+         "overDeviceLimit" : false,
+         "expirationIntent" : null,
+         "__typename" : "AppStoreSubscription",
+         "user" : null,
+         "status" : "canceled",
+         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
+       },
+       "__typename" : "SubscriptionResult"
+    }
+  }
+};
+
+$done({body : JSON.stringify(anpple)});
